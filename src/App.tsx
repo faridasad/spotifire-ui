1885959920f506faadb14223179b60052596f4c0
faridasad <asadlifaridd@gmail.com>
2023-04-21 18:03:20 +0400

    Implement react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+// Components
+import Navbar from "./components/Navbar";
+import Topbar from "./components/Topbar";
+import Bottombar from "./components/Bottombar";
+
+// Pages
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+
+// Router
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 function App() {
-  return <div>Spoti 🔥</div>;
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Topbar />
+        <main>
+          <Outlet />
+        </main>
+        <Bottombar />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/search", element: <Search /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;

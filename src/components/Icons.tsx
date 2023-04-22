19cd2a0caeac2a2a08e0e267dd778ebd6860a4b8
faridasad <asadlifaridd@gmail.com>
2023-04-23 03:54:11 +0400

    Add volume icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -226,6 +226,24 @@ const QueueIcon = ({ size }: CustomIconProps) => {
   );
 };
 
+const VolumeIcon = ({ size }: CustomIconProps) => {
+  return (
+    <svg
+      role="presentation"
+      height={size}
+      width={size}
+      aria-hidden="true"
+      aria-label="Volume medium"
+      id="volume-icon"
+      viewBox="0 0 16 16"
+      data-encore-id="icon"
+      fill="currentColor"
+    >
+      <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 6.087a4.502 4.502 0 0 0 0-8.474v1.65a2.999 2.999 0 0 1 0 5.175v1.649z"></path>
+    </svg>
+  );
+};
+
 const icons: Record<string, FunctionComponent<CustomIconProps>> = {
   home: HomeIcon,
   logo: LogoIcon,
@@ -241,6 +259,7 @@ const icons: Record<string, FunctionComponent<CustomIconProps>> = {
   next: NextIcon,
   repeat: RepeatIcon,
   queue: QueueIcon,
+  volume: VolumeIcon,
 };
 
 type IconProps = {

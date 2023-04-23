9f955dad0f5ee97cb785f06ccce78dbe3af9280a
faridasad <asadlifaridd@gmail.com>
2023-04-24 01:16:27 +0400

    Collection filters

--- src/components/Topbar/Filters.tsx
+++ src/components/Topbar/Filters.tsx
@@ -1,9 +1,27 @@
-import './filters.scss'
+import { NavLink } from "react-router-dom";
+import "./filters.scss";
 
 function Filters() {
   return (
-    <div>Filters</div>
-  )
+    <div className="filters">
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/collection/playlists">Playlists</NavLink>
+          </li>
+          <li>
+            <NavLink to="/collection/podcasts">Podcasts</NavLink>
+          </li>
+          <li>
+            <NavLink to="/collection/artists">Artists</NavLink>
+          </li>
+          <li>
+            <NavLink to="/collection/albums">Albums</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default Filters
+export default Filters;

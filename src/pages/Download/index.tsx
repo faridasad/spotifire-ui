753ac3cc25da9276960dad596d8124a7e4efedb5
faridasad <asadlifaridd@gmail.com>
2023-04-24 14:04:03 +0400

    Download page

--- src/pages/Download/index.tsx
+++ src/pages/Download/index.tsx
@@ -1,9 +1,18 @@
-import './download.scss'
+import "./download.scss";
 
 function Download() {
   return (
-    <div>Download</div>
-  )
+    <div className="download-app">
+      <img src="https://open.spotifycdn.com/cdn/images/devices/mac.3fbeb8c6.png" />
+      <div className="texts">
+        <h1>
+          Seamlessly listen to music you love. Download the Spotify app for your
+          computer.
+        </h1>
+        <a>Get our free app</a>
+      </div>
+    </div>
+  );
 }
 
-export default Download
+export default Download;

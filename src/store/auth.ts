056c666bd5485e5536b9bd3fcd4f51fc06e2870e
faridasad <asadlifaridd@gmail.com>
2023-04-30 17:18:38 +0400

    Clean up and persist store

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type AuthState = {
   accessToken: string | null;
@@ -9,19 +10,47 @@ type AuthState = {
 
 type AuthAction = {
   login: (code: string | null) => void;
+  updateAccessToken: (accessToken: string) => void;
+  updateRefreshToken: (refreshToken: string) => void;
+  updateExpiresIn: (expiresIn: number) => void;
+  clear: () => void;
 };
 
-const useAuthStore = create<AuthState & AuthAction>((set) => ({
-  accessToken: null,
-  refreshToken: null,
-  expiresIn: null,
-  login: async (code) => {
-    const response = await axios.post("http://localhost:5000/login", {
-      code,
-    });
-    const { accessToken, refreshToken, expiresIn } = response.data;
-    set(() => ({ accessToken, refreshToken, expiresIn }));
-  },
-}));
+const useAuthStore = create<AuthState & AuthAction>()(
+  persist(
+    (set) => ({
+      code: null,
+      accessToken: null,
+      refreshToken: null,
+      expiresIn: null,
+      login: async (code) => {
+        const response = await axios.post("http://localhost:5000/login", {
+          code,
+        });
+        const { accessToken, refreshToken, expiresIn } = response.data;
+        set(() => ({ accessToken, refreshToken, expiresIn }));
+      },
+      updateAccessToken: (accessToken) => set(() => ({ accessToken })),
+      updateRefreshToken: (refreshToken) => set(() => ({ refreshToken })),
+      updateExpiresIn: (expiresIn) => set(() => ({ expiresIn })),
+      clear: async () => {
+        set(() => ({
+          code: null,
+          accessToken: null,
+          refreshToken: null,
+          expiresIn: null,
+        }));
+        try {
+          await axios.get("http://localhost:5000/logout");
+        } catch (err) {
+          window.location.reload();
+        }
+      },
+    }),
+    {
+      name: "auth-storage",
+    }
+  )
+);
 
 export default useAuthStore;
